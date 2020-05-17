346753a3f1dd8a3faf81e452814435b2c7f33e8b
Hakeem <hakeemsbuchanan@outlook.com>
2020-05-17 16:39:10 -0400

    update again

---
+++ build/precache-manifest.6139c9a938d27519291f1cdc53732bfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a7a30501558ed863a500fed95d868e",
+    "url": "/websitev2/index.html"
+  },
+  {
+    "revision": "67f71bf2f62683dbabcc",
+    "url": "/websitev2/static/css/main.502b28da.chunk.css"
+  },
+  {
+    "revision": "f1824c238e2e4ab02b19",
+    "url": "/websitev2/static/js/2.04a8d6ca.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/websitev2/static/js/2.04a8d6ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f71bf2f62683dbabcc",
+    "url": "/websitev2/static/js/main.ad71ef20.chunk.js"
+  },
+  {
+    "revision": "4a8881036d90208b10c2",
+    "url": "/websitev2/static/js/runtime-main.474f0ac4.js"
+  }
+]);
